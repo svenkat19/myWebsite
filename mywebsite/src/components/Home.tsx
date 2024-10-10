18e7d90063300a76cf3376194419f77c9ed68eeb
shashyv <shashyv@gmail.com>
2024-10-10 23:58:32 +0530

    Updated home page

--- mywebsite/src/components/Home.tsx
+++ mywebsite/src/components/Home.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <img 
-        src='signature-E58336090A248AA57D9FFA7A7F4640552.gif' 
-        style={{ width: '20em', height: '10em' }} // Inline CSS for width and height
-        alt="Signature" // Add alt attribute for accessibility
-      />
-    </div>
+    <>
+      <div className='satisfy-regular'>
+        
+        <div className="container" style={{ marginTop: '15rem' }}>
+          <h1>Hey there</h1>
+          <h1>Shashank Venkat here.</h1>
+        </div>
+        <div>
+<img src="tTBWByQwdtDAxTvZT4ADi-transformed.png" style={{height:'20rem', width:'19rem'}} alt="" />
+        </div>
+        
+      </div>
+      <div className="container find-me">
+        <p>WANNA CONNECT?</p>
+        <div className="social-links">
+          <a href="https://github.com/svenkat19" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" className="light-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/shashankvenkat18/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" className="light-icon" />
+          </a>
+          <a href="https://www.facebook.com/shaz.venk/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faFacebook} size="2x" className="light-icon" />
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 

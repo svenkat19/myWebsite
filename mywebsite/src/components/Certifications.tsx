0c248ae2bd234991f2c9787b8b5dcdf32078a1ab
shashyv <shashyv@gmail.com>
2024-10-23 15:21:18 +0530

    final push

--- mywebsite/src/components/Certifications.tsx
+++ mywebsite/src/components/Certifications.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import "./Certification.css"
+import React from 'react';
+import "./Certification.css";
+
 const Certifications = () => {
   return (
     <div className="project-section">
+      <h1>Get in Touch</h1>
+      E-mail : shashyv@gmail.com
     </div>
-  )
+  );
 }
 
-export default Certifications
+export default Certifications;
